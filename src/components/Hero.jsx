10686efb6d71322f5968ade1650c5fe45600cc08
PREMSAITEJA <premsaiteja58@gmail.com>
2025-05-19 21:36:47 +0530

    pythogorous commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
@@ -32,11 +32,11 @@ const Hero = () => {
         </div>
       </div>
       {isMobile ? (
-        <img src="/p1.png" alt="Computer" className="mx-auto w-2/3" />
+        // <img src="/p1.png" alt="Computer" className="mx-auto w-2/3" />
+      null
       ) : (
         <ComputersCanvas />
       )}
-      {isMobile ? null : <ComputersCanvas />}
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
